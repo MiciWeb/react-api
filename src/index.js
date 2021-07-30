2e1e6694a61a39bd359c5fb1eda52ba41c801a7f
Micipsa <micipsa.sersour@epitech.eu git config --global user.name mici92exitcd>
2021-07-30 10:03:25 +0200

    deploy my react api to github page

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import api from "./components/api.json";
+
+class ProductCategoryRow extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+class ProductRow extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+class ProductTable extends React.Component {
+    render() {
+        return (
+            <div>
+                {console.log(props.children)}
+            </div>
+        )
+    }
+}
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="search" placeholder="search" />
+                    <p>
+                        <input type="checkbox" />
+                        {" "} Only show products in stock
+                    </p>
+                </form>
+            </div>
+        )
+    }
+}
+
+class Table extends React.Component {
+    render() {
+        return (
+            <div>
+                 <SearchBar />
+                 <ProductTable products = {this.props.products} />
+            </div>
+        )
+    }
+}
+
+
+const PRODUCTS = api;
+
+ReactDOM.render(
+    <Table products={PRODUCTS} />,
+    document.getElementById('root')
+);
